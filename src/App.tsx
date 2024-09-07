e174565f0141ecb0e6006ee7a5e23c6788030418
Divine Obeten <divineobeten9@gmail.com>
2024-09-07 12:05:06 +0100

    delete changes

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="w-screen h-screen flex items-center justify-center text-red-700 text-4xl">
-      TAILWIND WORKS!
-    </div>
-  );
-}
-
-export default App;
