7d54eafe12d4dc513f43b79845b7bb49d8f4f334
Divine Obeten <divineobeten9@gmail.com>
2024-09-11 20:22:35 +0100

    sign up page

--- src/Auth/Signup.jsx
+++ src/Auth/Signup.jsx
@@ -1,9 +1,144 @@
-import React from 'react'
+import React from "react";
+import signupBG from "../assets/signupBG.svg";
+import Google from "../assets/google.svg";
+import Twitter from "../assets/twitter.svg";
+import Wallets from "../assets/wallets.svg";
+import Check from "../assets/check.svg";
+import Eye from "../assets/eye.svg";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="w-screen flex bg-mobileBackground justify-center lg:justify-normal ">
+      <div className="hidden lg:flex w-1/2 h-screen fixed">
+        <img src={signupBG} alt="" className="w-full" />
+      </div>
+      <div className="max-w-xl lg:max-w-none w-screen lg:w-1/2 min-h-screen text-white px-5 py-10 lg:py-20 relative lg:left-1/2 flex justify-center">
+        <div className="w-11/12 lg:w-2/3">
+          <div className="mb-8 text-center">
+            <h1 className="text-3xl lg:text-4xl lg:mb-2 font-semibold">
+              Let's get started
+            </h1>
+            <p className="text-textGray text-sm">
+              Have an account? <span className="text-PrimaryPurple">Login</span>{" "}
+              {/*the "login" would eventually become a link to the login page*/}
+            </p>
+          </div>
 
-export default Signup
+          <form className="w-full">
+            <div className="flex flex-col mb-5">
+              <label htmlFor="first name" className="text-lg mb-2">
+                First Name
+              </label>
+              <input
+                type="text"
+                placeholder="Input"
+                className="bg-inputBackground border-inputBorderColor border-solid border-2 rounded-md h-16 px-2 placeholder:text-lg placeholder:text-gray-300 outline-none focus:border-PrimaryPurple focus:border-2"
+              />
+            </div>
+            <div className="flex flex-col mb-5">
+              <label htmlFor="last name" className="text-lg mb-2">
+                Last Name
+              </label>
+              <input
+                type="text"
+                placeholder="Input"
+                className="bg-inputBackground border-inputBorderColor border-solid border-2 rounded-md h-16 px-2 placeholder:text-lg placeholder:text-gray-300 outline-none focus:border-PrimaryPurple focus:border-2"
+              />
+            </div>
+            <div className="flex flex-col mb-5">
+              <label htmlFor="first name" className="text-lg mb-2">
+                Field
+              </label>
+              <select
+                name="field"
+                id=""
+                className="bg-inputBackground border-inputBorderColor border-solid border-2 rounded-md h-16 px-2 placeholder:text-lg placeholder:text-gray-300 outline-none focus:border-PrimaryPurple focus:border-2"
+              >
+                <option value="UI/UX Design">UI/UX Design</option>
+              </select>
+            </div>
+            <div className="flex flex-col mb-5">
+              <label htmlFor="company id" className="text-lg mb-2">
+                Company ID
+              </label>
+              <input
+                type="text"
+                placeholder="Input"
+                className="bg-inputBackground border-inputBorderColor border-solid border-2 rounded-md h-16 px-2 placeholder:text-lg placeholder:text-gray-300 outline-none focus:border-PrimaryPurple focus:border-2"
+              />
+            </div>
+            <div className="flex flex-col mb-5">
+              <label htmlFor="email address" className="text-lg mb-2">
+                Email Address
+              </label>
+              <input
+                type="email"
+                placeholder="Input"
+                className="bg-inputBackground border-inputBorderColor border-solid border-2 rounded-md h-16 px-2 placeholder:text-lg placeholder:text-gray-300 outline-none focus:border-PrimaryPurple focus:border-2"
+              />
+            </div>
+            <div className="flex flex-col mb-5">
+              <label htmlFor="password" className="text-lg mb-2">
+                Password
+              </label>
+              <div className="relative w-full">
+                <input
+                  type="password"
+                  placeholder="Input"
+                  className="bg-inputBackground border-inputBorderColor border-solid border-2 rounded-md h-16 px-2 placeholder:text-lg placeholder:text-gray-300 outline-none focus:border-inputborderGreen focus:border-2 w-full"
+                />
+                <img
+                  src={Eye}
+                  alt=""
+                  className="absolute top-[22px] right-6 cursor-pointer"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col mb-5 w-full">
+              <label htmlFor="confirm password" className="text-lg mb-2">
+                Confirm Password
+              </label>
+              <div className="relative w-full h-max">
+                <input
+                  type="Password"
+                  placeholder="Input"
+                  className="bg-inputBackground border-inputBorderColor border-solid border-2 rounded-md h-16 px-2 placeholder:text-lg placeholder:text-gray-300 outline-none focus:border-inputborderGreen focus:border-2 w-full"
+                />
+                <img
+                  src={Check}
+                  alt=""
+                  className="absolute top-[22px] right-6 cursor-pointer"
+                />
+              </div>
+            </div>
+
+            <button className="bg-PrimaryPurple h-16 rounded-lg w-full mt-6 font-semibold text-xl outline-none">
+              Sign Up
+            </button>
+          </form>
+          <div className="flex items-center gap-3 mt-10 text-textGray">
+            <div className="w-5 bg-[#333333] flex-grow h-[1px]"></div>
+            Or
+            <div className="w-5 bg-[#333333] flex-grow h-[1px]"></div>
+          </div>
+          <div className="flex mt-10 gap-3">
+            <button className="bg-inputBackground border-inputBorderColor border-2 border-solid rounded-md h-16 flex-grow text-lg font-semibold flex items-center justify-center gap-2 outline-none">
+              <img src={Google} alt="" />
+              Google
+            </button>
+            <button className="bg-inputBackground border-inputBorderColor border-2 border-solid rounded-md h-16 flex-grow text-lg font-semibold flex items-center justify-center gap-2 outline-none">
+              <img src={Twitter} alt="" />
+              Twitter
+            </button>
+            <button className="hidden bg-inputBackground border-inputBorderColor border-2 border-solid rounded-md h-16 flex-grow text-lg font-semibold lg:flex items-center justify-center gap-2 outline-none">
+              <img src={Wallets} alt="" />
+              Wallet
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mobileBackground: "#161616",
+        textGray: "#888888",
+        inputBackground: "#222222",
+        inputBorderColor: "#333333",
+        PrimaryPurple: "#8E73EF",
+        inputborderGreen: "#5FC381",
+      },
+    },
   },
   plugins: [],
 };
